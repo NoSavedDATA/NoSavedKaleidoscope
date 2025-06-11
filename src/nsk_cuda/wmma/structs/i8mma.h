9e6f55a4dbb77f1107df1a3a20e9925e9f3d6751
nosaveddata <nosaveddata@Z1.localdomain>
2025-06-11 11:36:13 -0300

    sync

--- src/nsk_cuda/wmma/structs/i8mma.h
+++ src/nsk_cuda/wmma/structs/i8mma.h
@@ -1,76 +1,25 @@
 #pragma once
 
-#include <mma.h>
-
-#include "../../structs/i8_wmma_frags.h"
-#include "ptx.h"
-
-using namespace nvcuda;
+#include "../../structs/smem_cpasync_loader.h"
+#include "../../structs/wmma_indexes.h"
 
 
 
-#define LDMATRIX_X2(R0, R1, addr) \
-    asm volatile("ldmatrix.sync.aligned.x2.m8n8.shared.b16 {%0, %1}, [%2];\n" : "=r"(R0), "=r"(R1) : "r"(addr))
-
-#define LDMATRIX_X4(R0, R1, R2, R3, addr)                                             \
-    asm volatile("ldmatrix.sync.aligned.x4.m8n8.shared.b16 {%0, %1, %2, %3}, [%4];\n" \
-                 : "=r"(R0), "=r"(R1), "=r"(R2), "=r"(R3)                             \
-                 : "r"(addr))
-
-
-template<int warp_rows_per_m, int warp_cols_per_n>
-__device__ inline void warp_tiled_wmma_i8_16x16x16_mma(int *out, int *reg_A, int *reg_B,
-                                              wmma_indexes<warp_rows_per_m, warp_cols_per_n>& wmma_idx,
-                                              const int M, const int N, const int WMMA_M, const int WMMA_N)
-{
-
-
-    // for (int wx_tile=0; wx_tile<1; ++wx_tile)
-    for (int wy_tile=0; wy_tile<warp_cols_per_n; ++wy_tile)
-    { 
-        // for (int wy_tile=0; wy_tile<1; ++wy_tile)
-        for (int wx_tile=0; wx_tile<warp_rows_per_m; ++wx_tile)
-        {
-            size_t j_s = (wy_tile % 2) ? (warp_rows_per_m - wx_tile - 1) : wx_tile;
-            // size_t j_s = wx_tile;
-
-            if ((wmma_idx.block_y*wmma_idx.blocking_size_y + wy_tile*WMMA_M)<M && (wmma_idx.block_x*wmma_idx.blocking_size_x + j_s*WMMA_N)<N)
-                wmma16x16x16_i8_mma(out+(j_s*warp_cols_per_n + wy_tile)*8, reg_A+wy_tile*4, reg_B+j_s*4); // 8 is the frag ld  
-        }
-    }
-    // __syncthreads();
-    
-    // if(threadIdx.x==0)
-    // {
-    //     printf("out is: ");
-    //     for (int i=0;i<8;++i)
-    //         printf("%d, ", out[i]);
-    //     printf("\n");
-
-    // }
-    
-
-}
-
-
-
-
+// TOO SLOW. REGISTERS BUG
 
 
 template<int warp_rows_per_m, int warp_cols_per_n, typename T>
-__device__ inline void wmma_i8_m16n16k16_mma(int *out,
-                                        wmma_indexes<warp_rows_per_m, warp_cols_per_n>& wmma_idx,
-                                        smem_cpasync_wmma_loader<warp_rows_per_m, warp_cols_per_n, T>& smem_loader,
-                                        float *x_smem, float *w_smem,
-                                        const int M, const int N, const int K,
-                                        const int WMMA_M, const int WMMA_N,
-                                        int chunks)
-{
-    // chunks = 1;
+struct INT8MMA {
+
+    int reg_A[2][warp_cols_per_n][4];
+    int reg_B[2][warp_rows_per_m][4];
+    int O[warp_rows_per_m][warp_cols_per_n][8];
+
     size_t reg_store_idx = 0;
     size_t reg_load_idx = 0;
 
-    int O[warp_rows_per_m][warp_cols_per_n][8];
+    __device__ INT8MMA() {
+
 #pragma unroll
     for (int i=0; i<warp_rows_per_m; ++i)
 #pragma unroll
@@ -78,22 +27,23 @@ __device__ inline void wmma_i8_m16n16k16_mma(int *out,
 #pragma unroll
             for (int k=0; k<8; ++k)
                 O[i][j][k] = 0;
+    }
 
 
-    int reg_A[2][warp_cols_per_n][4];
-    int reg_B[2][warp_rows_per_m][4];
+    __device__ void swap() {
+        reg_store_idx ^= 1;
+        reg_load_idx ^= 1;
+    }
 
 
-#pragma unroll
-    for (int k_stride=0; k_stride<chunks; ++k_stride)
+
+
+    __device__ void load_A(float *x_smem,
+                           wmma_indexes<warp_rows_per_m, warp_cols_per_n>& wmma_idx,
+                           smem_cpasync_wmma_loader<warp_rows_per_m, warp_cols_per_n, T>& smem_loader,
+                           const int WMMA_N, int k_stride) 
+                                        
     {
-        // smem_loader.store_frag_A(frag_loader, x_smem, WMMA_M, k_stride);
-        // smem_loader.store_frag_B(frag_loader, w_smem, WMMA_N, k_stride);
-
-
-        // if (threadIdx.x<32)
-        // {
-            
 #pragma unroll
         for (int i=0; i<warp_cols_per_n; ++i)
         {
@@ -108,8 +58,17 @@ __device__ inline void wmma_i8_m16n16k16_mma(int *out,
                     : "=r"(reg_A[reg_store_idx][i][0]), "=r"(reg_A[reg_store_idx][i][1])
                     : "r"(x_smem_casted));
         }
-                    
+    }
 
+
+
+
+    __device__ void load_B(float *w_smem,
+                           wmma_indexes<warp_rows_per_m, warp_cols_per_n>& wmma_idx,
+                           smem_cpasync_wmma_loader<warp_rows_per_m, warp_cols_per_n, T>& smem_loader,
+                           const int WMMA_M, int k_stride) 
+                                        
+    {
 #pragma unroll
         for (int i=0; i<warp_rows_per_m; ++i)
         {
@@ -125,17 +84,17 @@ __device__ inline void wmma_i8_m16n16k16_mma(int *out,
                     : "=r"(reg_B[reg_store_idx][i][0]), "=r"(reg_B[reg_store_idx][i][1])
                     : "r"(w_smem_casted));
         }
-
-        reg_store_idx ^= 1;
-        reg_load_idx ^= 1;
+    }
 
 
 
 
+    __device__ void mma() {
 
 #pragma unroll
         for (int wy_tile=0; wy_tile<warp_cols_per_n; ++wy_tile)
         { 
+            // for (int wy_tile=0; wy_tile<1; ++wy_tile)
 #pragma unroll
             for (int wx_tile=0; wx_tile<warp_rows_per_m; ++wx_tile)
             {
@@ -187,11 +146,6 @@ __device__ inline void wmma_i8_m16n16k16_mma(int *out,
             }
         }
     }
+    
 
-#pragma unroll
-        for (int wy_tile=0; wy_tile<warp_cols_per_n; ++wy_tile)
-#pragma unroll
-            for (int wx_tile=0; wx_tile<warp_rows_per_m; ++wx_tile)
-                for (int i=0;i<8;++i)
-                    out[(wx_tile*warp_cols_per_n + wy_tile)*8 + i] = O[wx_tile][wy_tile][i];
-}
+};
